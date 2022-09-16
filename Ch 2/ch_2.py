bf36550f0d6e952a269e25d6737e14c670a089f4
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-09-16 12:15:55 -0400

    Add files via upload

---
+++ Ch 2/ch_2.py
@@ -0,0 +1,25 @@
+'''
+Chapter 2. Python Language Basics, IPython, and Jupyter Notebooks
+
+McKinney, Wes. Python for Data Analysis (p. 39). O'Reilly Media. Kindle Edition. 
+'''
+
+# 2.2 IPython Basics
+
+# McKinney, Wes. Python for Data Analysis (p. 42). O'Reilly Media. Kindle Edition. 
+
+import numpy as np
+
+data = [np.random.standard_normal() for i in range(7)]
+
+data
+
+# ex:
+
+b = [1,2,3]
+help(b)
+
+# 2.3 Python Language Basics
+
+# McKinney, Wes. Python for Data Analysis (p. 53). O'Reilly Media. Kindle Edition. 
+
