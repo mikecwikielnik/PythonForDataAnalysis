6430920801a655198bbd387bd36cea0dff1f7a72
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-10-15 22:35:29 -0400

    Add files via upload

--- Ch 7/ch_7.py
+++ Ch 7/ch_7.py
@@ -1,43 +1,32 @@
 """
 Chapter 7. Data Cleaning and Preparation
 
-McKinney, Wes. Python for Data Analysis (p. 330). O'Reilly Media. Kindle Edition. 
+McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 """
-
-from sys import float_info
-import pandas as pd
 
 import numpy as np
 
-"""
-7.1 Handling Missing Data
+import pandas as pd
 
-McKinney, Wes. Python for Data Analysis (p. 330). O'Reilly Media. Kindle Edition. 
-"""
+# 7.1 Handling Missing Data
 
-# descriptive stats on pandas objects exclude missing data by default
-
-# ex:
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
 float_data = pd.Series([1.2, -3.5, np.nan, 0])
 
 float_data
 
-# ex: isna method
+# ex: isna() yields a Boolean Series with True where values are null
 
-float_data.isna()   # second nature
+float_data.isna()
 
-# ex: analysis on the missing data itself
+# ex: analysis done on the missing data itself
 
 string_data = pd.Series(["aardvark", np.nan, None, "avocado"])
 
-string_data     # create a series..second nature
-
-# isna() check
+string_data     # second nature
 
 string_data.isna()
-
-# ex: 
 
 float_data = pd.Series([1, 2, None], dtype='float64')
 
@@ -45,11 +34,11 @@ float_data
 
 float_data.isna()
 
-# Table 7-1. NA handling object methods
+# See Table 7-1 for a list of some functions related to missing data handling.
 
-# McKinney, Wes. Python for Data Analysis (p. 333). O'Reilly Media. Kindle Edition. 
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
 # Filtering Out Missing Data
 
-# McKinney, Wes. Python for Data Analysis (p. 333). O'Reilly Media. Kindle Edition. 
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
