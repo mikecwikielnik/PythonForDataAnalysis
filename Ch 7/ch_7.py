426cb4c8968147878df8d15f709bab4ddba57ddd
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-10-15 23:10:44 -0400

    Add files via upload

--- Ch 7/ch_7.py
+++ Ch 7/ch_7.py
@@ -42,3 +42,50 @@ float_data.isna()
 
 # McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
+# ex: dropna() is useful
+
+data = pd.Series([1, np.nan, 3.5, np.nan, 7])
+
+data.dropna()
+
+# below is the same
+
+data[data.notna()]
+
+# dropna() by default drops any row containing a missing value
+
+data = pd.DataFrame([[1., 6.5, 3.], [1., np.nan, np.nan],
+                    [np.nan, np.nan, np.nan], [np.nan, 6.5, 3.]])
+
+data
+
+# ex: hot="all" will drop only rows that are all NaN
+
+data.dropna(how="all")
+
+# to pass columns in the same way, pass axis="columns":
+
+data[4] = np.nan
+
+data
+
+data.dropna(axis="columns", how="all")
+
+# ex: Suppose you want to keep only rows containing at most a certain number of missing obs. thresh arg
+
+df = pd.DataFrame(np.random.standard_normal((7, 3)))
+
+df.iloc[:4, 1] = np.nan
+
+df.iloc[:2, 2] = np.nan
+
+df
+
+df.dropna()
+
+df.dropna(thresh=2)
+
+# Filling In Missing Data
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
