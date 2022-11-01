1d3c719ee7a60864b73d727a4727aa123ccdb639
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-11-01 18:45:38 -0400

    Add files via upload

--- Ch 8/ch_8.py
+++ Ch 8/ch_8.py
@@ -365,3 +365,41 @@ pd.concat([df1, df2], axis="columns", keys=["level1", "level2"], names=["upper",
 
 # McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
+# ex: Numpy's where fun, does the array-oriented equivalent of an if-else expression
+
+a = pd.Series([np.nan, 2.5, 0.0, 3.5, 4.5, np.nan],
+                index=["f", "e", "d", "c", "b", "a"])
+
+b = pd.Series([0., np.nan, 2., np.nan, np.nan, 5.],
+                index=["a", "b", "c", "d", "e", "f"])
+
+a 
+
+b
+
+np.where(pd.isna(a), b, a)
+
+# ex: to line up values by index, use the series combine_first method:
+
+a.combine_first(b)      # b into a, not a into b
+
+# ex: with dataframes, combine_first does the same thing col by col
+
+df1 = pd.DataFrame({"a": [1., np.nan, 5., np.nan],
+                    "b": [np.nan, 2., np.nan, 6.,],
+                    "c": range(2, 18, 4)})
+
+df2 = pd.DataFrame({"a": [5., 4., np.nan, 3., 7.],
+                    "b": [np.nan, 3., 4., 6., 8.]})
+
+
+df1 
+
+df2 
+
+df1.combine_first(df2)  # again, df2 into df1, not df1 into df2
+
+# 8.3 Reshaping and Pivoting
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
