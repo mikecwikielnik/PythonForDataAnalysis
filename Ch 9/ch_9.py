509274edc2008fd1de6628a0c14392db767d1b91
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-11-08 15:47:02 -0500

    Add files via upload

--- Ch 9/ch_9.py
+++ Ch 9/ch_9.py
@@ -32,3 +32,37 @@ plt.plot(data2)     # same here
 
 # McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
+# plots in matplotlib reside in a figure object
+
+import matplotlib.pyplot as plt     # nuance of interactive nb, you have to do this everytime
+
+fig = plt.figure()
+
+# you have to create one or more sub plots using add_subplots
+
+ax1 = fig.add_subplot(2, 2, 1)
+
+# this means that the figure should be 2x2 (so up to four plots in total)
+
+ax2 = fig.add_subplot(2, 2, 2)
+
+ax3 = fig.add_subplot(2, 2, 3)
+
+# plt.show()  # for the terminal, it seems you have to run this separately
+
+# ex: making a line plot with the plot method
+
+ax3.plot(np.random.standard_normal(50).cumsum(), color="black", linestyle="dashed")
+
+# plt.show()  # remember just ONE plt.show()
+
+ax1.hist(np.random.standard_normal(100), bins=20, color="black", alpha=0.3);
+
+ax2.scatter(np.arange(30), np.arange(30) + 3 * np.random.standard_normal(30));
+
+plt.show()
+
+# Adjusting the spacing around subplots
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
