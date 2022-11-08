9172a2bf2da8a92c11f98af12ecb707202b607b1
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-11-08 14:58:23 -0500

    Add files via upload

---
+++ Ch 9/ch_9.py
@@ -0,0 +1,34 @@
+"""
+Chapter 9. Plotting and Visualization
+
+McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+"""
+
+# 9.1 A Brief matplotlib API Primer
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
+import pandas as pd
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# ex: a line plot
+
+data = np.arange(10)
+
+data
+
+plt.plot(data)  # run in interactive window...aka jupyter nb
+
+data2 = np.arange(35)
+
+data2
+
+plt.plot(data2)     # same here
+
+# Figures and Subplots
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
