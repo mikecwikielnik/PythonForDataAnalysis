b14d32d3516e53eff6ae19e12eab07fd6ae07367
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-11-08 19:12:35 -0500

    Add files via upload

--- Ch 9/ch_9.py
+++ Ch 9/ch_9.py
@@ -136,3 +136,65 @@ ax.set(title="My first matplotlib plot", xlabel="Stages")   # is another way to 
 
 # McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
+# ex: easiest way is to pass the label arg when adding each piece of the plot
+
+fig, ax = plt.subplots()    # fig == canvas, ax == data
+
+ax.plot(np.random.standard_normal(1000).cumsum(), color="black", label="one");
+ax.plot(np.random.standard_normal(1000).cumsum(), color="black", linestyle="dashed", label="two");
+ax.plot(np.random.standard_normal(1000).cumsum(), color="black", linestyle="dotted", label="three");
+
+ax.legend()
+
+# Annotations and Drawing on a Subplot
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
+# ax.text(x, y, "Hello world!", family="monospace", fontsiz=10)
+
+# ex: plot the closing s&p 500 index 
+
+from datetime import datetime
+import pandas as pd
+
+fig, ax = plt.subplots()
+
+data = pd.read_csv("../book files/examples/spx.csv", index_col=0, parse_dates=True)
+spx = data["SPX"]
+
+spx.plot(ax=ax, color="black")
+
+crisis_data = [
+    (datetime(2007, 10, 11), "Peak of bull market"),
+    (datetime(2008, 3, 12), "Bear Stearns fails"),
+    (datetime(2008, 9, 15), "Lehman Bankruptcy")
+]
+
+for date, label in crisis_data:
+    ax.annotate(label, xy=(date, spx.asof(date) + 75),
+                xytext=(date, spx.asof(date) + 225),
+                arrowprops=dict(facecolor="black", headwidth=4, width=2, headlength=4),
+                horizontalalignment="left", verticalalignment="top")
+
+# Zoom in on 2007 - 2010
+
+ax.set_xlim(["1/1/2007", "1/1/2011"])
+ax.set_ylim([600, 1800])
+ax.set_title("Important dates in the 2008-2009 financial crisis")
+
+# ex: to add a shape to a plot
+
+fig, ax = plt.subplots()
+
+rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color="black", alpha=0.3)
+circ = plt.Circle((0.7, 0.2), 0.15, color="Blue", alpha=0.3)
+pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]], color="green", alpha=0.5)
+
+ax.add_patch(rect)
+ax.add_patch(circ)
+ax.add_patch(pgon)
+
+# Saving Plots to File
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
