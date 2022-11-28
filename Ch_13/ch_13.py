a840a53d3d12f8259d44abee209f5cd5cc5eddad
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-11-27 23:02:00 -0500

    Add files via upload

--- Ch_13/ch_13.py
+++ Ch_13/ch_13.py
@@ -30,7 +30,7 @@ with open(path) as f:
 
 records[0]
 
-# Counting Time Zones in Pure Python
+# Counting Time Zones in Pure Python, Example 1
 
 # McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
@@ -47,7 +47,7 @@ time_zones = [rec["tz"] for rec in records if "tz" in rec]
 
 time_zones[:10]
 
-# Next, we show two ways to produce counts by time zone: the hard way (using just the python lib) and the easy way (using pandas)
+# Next, we show two ways to produce counts by time zone: 1) the hard way (using just the python lib) and 2) the easy way (using pandas)
 
 # one way to do the counting is to use a dict to store counts while
 # we iterate through the time zones
@@ -101,6 +101,137 @@ counts = Counter(time_zones)
 counts.most_common(10)  # notice the empty string with 521 observations
 
 # Counting Time Zones with pandas
+
+# McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
+
+# you can create a df from the original set of records by passing
+# the list of records to pandas.DataFrame
+
+frame = pd.DataFrame(records)
+
+# we can look at basic info w/ frame.info()
+
+frame.info()
+
+frame["tz"].head()
+
+# use valu_counts method for Series
+
+tz_counts = frame["tz"].value_counts()
+
+tz_counts.head()
+
+# using matplotlib, we make plots nicer by filling in a sub value for unk or missing time zone values
+# we replace the missing values with fillna method and use boolean array indexing for the empty strings
+
+clean_tz = frame["tz"].fillna("Missing")    # lines 127 to 131 are very important
+
+clean_tz[clean_tz == ""] = "Unknown"
+
+tz_counts = clean_tz.value_counts()
+
+tz_counts.head()    # this is dope af
+
+# at this point, we can use the seaborn package to make a horizontal bar plot 
+
+import seaborn as sns
+
+subset = tz_counts.head()
+
+sns.barplot(y=subset.index, x=subset.to_numpy())    # beautiful ass chart
+
+# subset.index is import because it is col name
+# to_numpy makes an arra (or series) of the data
+
+# the a field contains info about the browser, device, or application used to perform the URL shortening
+
+frame["a"][1]
+
+frame["a"][50]
+
+frame["a"][51][:50]     # long line
+
+# ex: split off the first token in the string (which is roughly the broswer capability) 
+# and make another summary of the user behavior
+
+results = pd.Series([x.split()[0] for x in frame["a"].dropna()])
+
+results.head()
+
+results.value_counts().head(8)
+
+# now spose, you want to decompose the top time zones into windows and non-windows users
+
+cframe = frame[frame["a"].notna()].copy()
+
+# we want to then compute a value for whether or not each row is windows
+
+cframe["os"] = np.where(cframe["a"].str.contains("Windows"),
+                        "Windows", "Not Windows")
+
+cframe["os"].head(5)
+
+# now group the data by its time zone col and this new list of operating systems
+
+by_tz_os = cframe.groupby(["tz", "os"])
+
+# group counts can be computed with size. This result is then reshaped into a table w/ unstack
+
+agg_counts = by_tz_os.size().unstack().fillna(0)
+
+agg_counts.head()
+
+# Finally, lets select the top overall time zones. 
+
+# I construct an indirect index array from the row_counts in agg_counts.
+# After computing the row counts with agg_counts.sum("columns"), 
+# I can call argsort() to obtain an index array that can be used to sort in asc
+
+indexer = agg_counts.sum("columns").argsort()
+
+indexer.values[:10]
+
+# use take to select the rows in that order, then slice off the last 10 rows (largest values)
+
+count_subset = agg_counts.take(indexer[-10:])
+
+count_subset
+
+# nlargest does the same thing
+
+agg_counts.sum(axis="columns").nlargest(10)
+
+# seaborns barplot fn
+# First, call count_subset.stack() and reset the index to rearrange the data for better compatibility
+
+count_subset = count_subset.stack()     # i think this means the bars are stacked per index
+
+count_subset.name = "total"
+
+count_subset = count_subset.reset_index()
+
+count_subset.head(10)
+
+sns.barplot(x="total", y="tz", hue="os", data=count_subset)     # very important
+
+# ex: normalize the percentages to sum to 1! :)
+
+def norm_total(group):
+    group["normed_total"] = group["total"] / group["total"].sum()
+    return group
+
+results = count_subset.groupby("tz").apply(norm_total)
+
+# below is the final visualization
+sns.barplot(x="normed_total", y="tz", hue="os", data=results)   # very important
+
+# ex: you could have computed the normalized sum more efficiently by using the transform method with groupby
+
+g = count_subset.groupby("tz")
+
+results2 = count_subset["total"] / g["total"].transform("sum")
+
+# 13.2 MovieLens 1M Dataset
 
 # McKinney, Wes. Python for Data Analysis . O'Reilly Media. Kindle Edition. 
 
